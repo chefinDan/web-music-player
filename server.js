17e22f7de9089bb128e29bb44859f55bc21490ce
chefinDan <greendan@oregonstate.edu>
2018-11-14 16:45:58 -0800

    public dir, node modules, package.json

--- server.js
+++ server.js
@@ -5,27 +5,13 @@
 const express = require('express');
 const stream = require('stream');
 
-function requestHandler(req, res) {
-  console.log('=== request was recieved');
-  console.log('  - method:', req.method);
-  console.log('  - url:', req.url);
-  // console.log('  - headers:', req.headers);
+const app = express();
+const PORT = process.env.PORT;
 
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  // res.setHeader('Content-Type', 'image/jpg');
-  // res.setHeader('Content-Type', 'application/js');
-
-  var file = fs.readFileSync('index.html', 'utf8', err => { if(err) throw err; });
-  res.write(file);
-  // res.write( fs.readFile('./server.js', 'utf8', err => { if(err) throw err; });
-  res.end();
-}
+app.use(express.static('public'));
 
 
-var server = http.createServer(requestHandler);
-server.listen(process.env.PORT, function (err) {
+app.listen(PORT, err => {
   if(err) throw err;
-    console.log('=== server is listening on port', process.env.PORT);
-  }
-);
+  console.log(' === server listening on port ', PORT);
+});
