e926114e3351a7112f4c18e4bfbbed42b0dfac05
chefinDan <greendan@oregonstate.edu>
2018-12-01 16:54:50 -0800

    asks for song dir when loaded, needs lyric adder, filterbox, style.

--- public/index.js
+++ public/index.js
@@ -1,2 +1,106 @@
 //Client side js here
 // alert('Client Side js loaded');
+
+function handleFileUpload() {
+
+  var filePath = document.getElementById('file-input').value;
+
+  if (!filePath.includes('/')) {
+    alert("Enter a valid path!");
+  } else {
+    var postRequest = new XMLHttpRequest();
+    postRequest.open('POST', '/songs/upload');
+
+    var requestBody = JSON.stringify({
+      path: filePath,
+    });
+
+    postRequest.setRequestHeader('Content-Type', 'application/json');
+    postRequest.send(requestBody);
+
+    postRequest.onreadystatechange=(e)=>{
+      if(postRequest.readyState == 4) {
+        console.log('ready');
+        var getRequest = new XMLHttpRequest();
+        // var requestURL = '/library';
+        getRequest.open('GET', '/library');
+        getRequest.send();
+      }
+      else {
+        console.log('not ready');
+      }
+    }
+    hideModal();
+  }
+
+}
+
+
+function showModal() {
+
+  var modal = document.getElementById('add-photo-modal');
+  var modalBackdrop = document.getElementById('modal-backdrop');
+
+  modal.classList.remove('hidden');
+  modalBackdrop.classList.remove('hidden');
+
+}
+
+
+function clearModalInputs() {
+
+  var modalInputElements = document.querySelectorAll('#add-photo-modal input')
+  for (var i = 0; i < modalInputElements.length; i++) {
+    modalInputElements[i].value = '';
+  }
+
+}
+
+
+function hideModal() {
+  // var getRequest = new XMLHttpRequest();
+  // var requestURL = '/';
+  // getRequest.open('GET', '/', true);
+  // getRequest.send();
+  // console.log(getRequest.readyState);
+
+  // var requestBody
+
+  // postRequest.setRequestHeader('Content-Type', 'text/plain');
+
+  var modal = document.getElementById('add-photo-modal');
+  var modalBackdrop = document.getElementById('modal-backdrop');
+
+  modal.classList.add('hidden');
+  modalBackdrop.classList.add('hidden');
+
+  clearModalInputs();
+
+
+
+}
+
+
+/*
+ * Wait until the DOM content is loaded, and then hook up UI interactions, etc.
+ */
+window.addEventListener('DOMContentLoaded', function () {
+
+
+
+  var addPhotoButton = document.getElementById('add-photo-button');
+  if (addPhotoButton) {
+    addPhotoButton.addEventListener('click', showModal);
+  }
+
+  var modalAcceptButton = document.getElementById('modal-accept');
+  if (modalAcceptButton) {
+    modalAcceptButton.addEventListener('click', handleFileUpload);
+  }
+
+  var modalHideButtons = document.getElementsByClassName('modal-hide-button');
+  for (var i = 0; i < modalHideButtons.length; i++) {
+    modalHideButtons[i].addEventListener('click', hideModal);
+  }
+
+});
--- server.js
+++ server.js
@@ -7,21 +7,80 @@ var exphbs = require('express-handlebars');
 var Handlebars = require('handlebars');
 const fs = require('fs');
 const ID3 = require('id3-parser');
+var bodyParser = require('body-parser');
 
 var PORT = process.env.PORT || 8000;
 
-app.engine('handlebars', exphbs());
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-
-
-const contentDir = path.join(__dirname, '../', 'Music/mp3');
 app.use(express.static('public'));
-app.use('/public/music/mp3', express.static(contentDir));
 
-//route method for root, used for testing
-app.get('/', function (req, res, next) {
-    res.render('listTemplate', {"data": trackdata} );
+app.use(bodyParser.json());
+
+var contentDir;
+var trackdata = [];
+
+app.get('/', (req, res, next) => {
+  if(!contentDir) {
+    res.status(200).render('uploadFiles');
+  }
+  else {}
+
 });
+
+app.get('/library', (req, res, next) => {
+  console.log('render landing page');
+  res.status(200).render('library', { tracks: trackdata });
+});
+
+app.post('/songs/upload', (req, res, next) => {
+  if(req.body && req.body.path) {
+    contentDir = req.body.path;
+    res.status(200).send("Success");
+    app.use(express.static(contentDir));
+    parseTrackData();
+  }
+  else {
+    res.status(400).send("Request needs a body with a path");
+  }
+});
+
+
+function parseTrackData() {
+  console.log(contentDir);
+    fs.readdir(contentDir, (err, files) => {
+      console.log(files);
+      files.forEach( elem => {
+        fs.readFile(path.join(contentDir, elem), (err, buf) => {
+          var tags = ID3.parse(buf);
+          // console.log(tags);
+          if(!tags.image) {
+            tags.image = {
+              "data": "null",
+              "mime": "null"
+            };
+          }
+          trackdata.push({"title": tags.title.trim(),
+                          "artist": tags.artist.trim(),
+                          "album": tags.album.trim(),
+                          "year": tags.year.trim(),
+                          "genre": tags.genre.trim(),
+                          "cover":
+                            {
+                              "img": Buffer.from(tags.image.data, 'base64'),
+                              "mime": tags.image.mime
+                            },
+                            "url": contentDir + elem
+          });
+          // fs.copyFile('source.txt', 'destination.txt', (err) => {
+          // if (err) throw err;
+          // console.log('source.txt was copied to destination.txt');
+          // });
+          console.log(trackdata);
+        });
+      });
+    });
+}
 
 
 
@@ -32,32 +91,8 @@ app.get('/', function (req, res, next) {
 // To use on our own computer, make a directory of only mp3's,
 // assign path to 'const contentDir' above
 
-var trackdata = [];
-fs.readdir(contentDir, (err, files) => {
-  files.forEach( elem => {
-    fs.readFile(path.join(contentDir, elem), (err, buf) => {
-      var tags = ID3.parse(buf);
-      if(!tags.image) {
-        tags.image = {
-          "data": "null",
-          "mime": "null"
-        };
-      }
-      trackdata.push({"title": tags.title,
-                      "artist": tags.artist,
-                      "album": tags.album,
-                      "year": tags.year,
-                      "genre": tags.genre,
-                      "cover":
-                        {
-                          "img": new Buffer(tags.image.data, 'base64'),
-                          "mime": tags.image.mime
-                        },
-                        "url": elem
-      });
-    });
-  });
-});
+
+
 
 
 //route method for img artwork, uses :album param and virtual paths!!
@@ -76,6 +111,7 @@ app.get('/public/img/:album', (req, res, next) => {
     next();
   }
 });
+
 
 //catches all instances where there is no album artwork
 app.get('/public/img/*', (req, res, next) => {
